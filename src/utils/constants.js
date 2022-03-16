1cebaa327f70a5b2570896a27c0b9b43170d40fb
Swanand-Wagh <swanandwagh7@gmail.com>
2022-03-17 00:23:55 +0530

    added basic util format structure

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from "./Charity.json";
+
+export const contract_ABI = abi.abi;
+
+// got after deploying the project
+export const contractAddress = "0xCeF554aa84c6C63b74129be7376442b26db74B08";
