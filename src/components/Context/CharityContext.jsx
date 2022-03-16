75db78a444ea6482fc7f5a45e4775e1c76306798
Swanand-Wagh <swanandwagh7@gmail.com>
2022-03-17 00:23:30 +0530

    Added toasts to Navbar

--- src/components/Context/CharityContext.jsx
+++ src/components/Context/CharityContext.jsx
@@ -1,16 +1,30 @@
 import React, { createContext, useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
+import { contract_ABI } from "../../utils/constants";
+import toast, { Toaster } from "react-hot-toast";
 
 const { ethereum } = window;
 export const CharityContext = createContext();
 
 export const CharityDAOProvider = ({ children }) => {
   const { web3, Moralis, user } = useMoralis();
+  const [contractABI, setContractABI] = useState(contract_ABI);
+  const [contractAddress, setContractAddress] = useState("");
+
+  const toastStyles = {
+    style: {
+      marginTop: "4.25rem",
+      fontWeight: "600",
+      background: "#333",
+      color: "#fff",
+    },
+    duration: 2500,
+  };
 
   useEffect(() => {
     const checkIfWalletIsConnected = async () => {
       try {
-        if (!ethereum) return alert("Please install MetaMask.");
+        if (!ethereum) return toast.error("Please install MetaMask.");
       } catch (error) {
         console.log(error);
       }
@@ -20,8 +34,19 @@ export const CharityDAOProvider = ({ children }) => {
   }, []);
 
   return (
-    <CharityContext.Provider value={{ }}>
-      {children}
-    </CharityContext.Provider>
+    <>
+      <Toaster />
+      <CharityContext.Provider
+        value={{
+          contractABI,
+          setContractABI,
+          contractAddress,
+          setContractAddress,
+          toastStyles,
+        }}
+      >
+        {children}
+      </CharityContext.Provider>
+    </>
   );
 };
--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Navbar.css";
 import siteLogo from "../../../assets/logo.png";
 import { useMoralis } from "react-moralis";
 import { useNavigate } from "react-router-dom";
+import { CharityContext } from "../../Context/CharityContext";
+import toast, { Toaster } from "react-hot-toast";
 
 const Navbar = ({ userType }) => {
+  const { toastStyles } = useContext(CharityContext);
   let navigate = useNavigate();
   const { authenticate, isAuthenticated, logout, user } = useMoralis();
   const [isNGO, setIsNGO] = useState(undefined);
@@ -14,8 +17,14 @@ const Navbar = ({ userType }) => {
 
   useEffect(() => {
     if (isNGO !== undefined) {
-      if (isAuthenticated && isNGO && userType === "DONOR") logout();
-      if (isAuthenticated && !isNGO && userType === "NGO") logout();
+      if (isAuthenticated && isNGO && userType === "DONOR") {
+        toast.error("Connect Again using a NGO Account!", toastStyles);
+        logout();
+      }
+      if (isAuthenticated && !isNGO && userType === "NGO") {
+        toast.error("Connect Again using a DONOR Account!!", toastStyles);
+        logout();
+      }
     }
   }, [isNGO]);
 
@@ -25,11 +34,9 @@ const Navbar = ({ userType }) => {
     }
   }, []);
 
-  const connectWallet = () => {
-    authenticate({
-      signingMessage: `You are About to Login as ${userType}`,
-    }).then((u) => {
-      const isNGO = u.get("isNgo");
+  const connectWallet = async () => {
+    authenticate().then((u) => {
+      const isNGO = u?.get("isNgo"); // remove ? if error
       setIsNGO(isNGO);
 
       if (isNGO === undefined) {
@@ -43,46 +50,57 @@ const Navbar = ({ userType }) => {
           navigate("/dashboard/ngo");
         }
       }
+      toast.success("Wallet Successfully Connected!", toastStyles);
     });
   };
 
   return (
-    <nav className="app__navbar flex__center section__padding">
-      <img
-        src={siteLogo}
-        title="Logo"
-        alt="logo"
-        onClick={() => navigate("/")}
-      />
-      <ul>
-        <li onClick={() => navigate("/")}>Home</li>
-        <li
-          onClick={() => navigate("/dashboard/ngo")}
-          style={userType ? hideElement : showElement}
-        >
-          NGO Dashboard
-        </li>
-        <li
-          onClick={() => navigate("/dashboard/donor")}
-          style={userType ? hideElement : showElement}
-        >
-          DONOR Dashboard
-        </li>
-        {userType && !isAuthenticated && (
-          <li onClick={() => connectWallet()}>Connect Wallet</li>
-        )}
-        {userType === "NGO" && isAuthenticated && <li>Host Campaign</li>}
-        {userType && isAuthenticated && (
-          <>
-            <li>
-              {userType} :{" "}
-              <span style={{ fontWeight: "500" }}>{user.getUsername()}</span>
-            </li>
-            <li onClick={() => logout()}>Logout</li>
-          </>
-        )}
-      </ul>
-    </nav>
+    <>
+      <Toaster />
+      <nav className="app__navbar flex__center section__padding">
+        <img
+          src={siteLogo}
+          title="Logo"
+          alt="logo"
+          onClick={() => navigate("/")}
+        />
+        <ul>
+          <li onClick={() => navigate("/")}>Home</li>
+          <li
+            onClick={() => navigate("/dashboard/ngo")}
+            style={userType ? hideElement : showElement}
+          >
+            NGO Dashboard
+          </li>
+          <li
+            onClick={() => navigate("/dashboard/donor")}
+            style={userType ? hideElement : showElement}
+          >
+            DONOR Dashboard
+          </li>
+          {userType && !isAuthenticated && (
+            <li onClick={() => connectWallet()}>Connect Wallet</li>
+          )}
+          {userType === "NGO" && isAuthenticated && <li>Host Campaign</li>}
+          {userType && isAuthenticated && (
+            <>
+              <li>
+                {userType} :{" "}
+                <span style={{ fontWeight: "500" }}>{user.getUsername()}</span>
+              </li>
+              <li
+                onClick={() => {
+                  toast.error("Wallet Disconnected!", toastStyles);
+                  logout();
+                }}
+              >
+                Logout
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </>
   );
 };
 
