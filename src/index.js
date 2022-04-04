dd527f3f5de494f6a1d6719b87ccfe0c41376bdf
Swanand-Wagh <swanandwagh7@gmail.com>
2022-04-04 11:20:24 +0530

    used .env instead of moralis_config.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MoralisProvider } from 'react-moralis';
 import App from './App';
-import { APP_ID, SERVER_URL } from './moralis_config';
 import { BrowserRouter } from 'react-router-dom';
 import { CharityDAOProvider } from './components/Context/CharityContext';
 
 ReactDOM.render(
-  <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+  <MoralisProvider
+    appId={process.env.REACT_APP_MORALIS_APPLICATION_ID}
+    serverUrl={process.env.REACT_APP_MORALIS_SERVER_URL}
+  >
     <CharityDAOProvider>
       <BrowserRouter>
         <App />
